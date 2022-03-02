39158f2b3f70aa79ec2928e84c97b7e5eb757a01
Ethan <ethanjsummers00@gmail.com>
2022-03-02 12:05:01 -0700

    Div main to main
    - Using <main> instead of <div id="main">

--- scripts.js
+++ scripts.js
@@ -11,3 +11,15 @@ else {
 }
 
 document.getElementById("myAge").innerHTML = age;
+
+var slideout = new Slideout({
+    'panel': document.getElementById('panel'),
+    'menu': document.getElementById('menu'),
+    'padding': 256,
+    'tolerance': 70
+  });
+
+  // Toggle button
+  document.querySelector('.toggle-button').addEventListener('click', function() {
+    slideout.toggle();
+  });
