6708e4fdf1f551e523d7305199cf7fa80101ab0d
Ethan Summers <ethanjsummers00@gmail.com>
2023-07-11 14:03:31 -0700

    Navigation Redesign/Adding Contact Form

--- scripts.js
+++ scripts.js
@@ -1,3 +1,11 @@
+const strongColor = document.querySelector('strong');
+const footColor = document.querySelector('footer');
+const aboutSectionStatus = document.querySelector('#aboutPage');
+const portfolioPageStatus = document.querySelector('#portfolioPage');
+const contactPageStatus = document.querySelector('#contactPage');
+const aboutTab = document.getElementById('aboutTab')
+const portfolioTab = document.getElementById('portfolioTab')
+
 var year = (new Date()).getFullYear();
 var month = (new Date()).getMonth();
 var day = (new Date()).getDate();
@@ -47,31 +55,61 @@ $(document).ready(function(){
   });
 
 // changes color and page content depending on the state of the switch
-function pageSwitch() {
-    const strongColor = document.querySelector('strong');
-    const footColor = document.querySelector('footer');
-    const aboutSectionStatus = document.querySelector('#aboutPage');
-    const portfolioPageStatus = document.querySelector('#portfolioPage');
-    const headBlock = document.querySelector('#headBlock');
+// function pageSwitch() {
+//     let pageNum = 0;
 
-    if (document.getElementById("pageSwitch").checked == true) {
-        strongColor.style.backgroundColor = "var(--blue)";
-        footColor.style.backgroundColor = "var(--blue)";
-        aboutSectionStatus.style.display = "none";
-        portfolioPageStatus.style.display = "block";
-        headBlock.style.display = "none";
-        // document.getElementById("orn").style.color = "#000000";
-        // document.getElementById("blu").style.color = "var(--blue)";
-    }
-    if (document.getElementById("pageSwitch").checked == false){
-        strongColor.style.backgroundColor = "var(--orange)";
-        footColor.style.backgroundColor = "var(--orange)";
-        aboutSectionStatus.style.display = "block";
-        portfolioPageStatus.style.display = "none";
-        headBlock.style.display = "block";
-        // document.getElementById("orn").style.color = "var(--orange)";
-        // document.getElementById("blu").style.color = "#000000";
-    }
 
-    console.log(document.getElementById("pageSwitch").checked);
+//     if (document.getElementById("pageSwitch").checked == true) {
+//         strongColor.style.backgroundColor = "var(--blue)";
+//         footColor.style.backgroundColor = "var(--blue)";
+//         aboutSectionStatus.style.display = "none";
+//         portfolioPageStatus.style.display = "block";
+//         headBlock.style.display = "none";
+//         // document.getElementById("orn").style.color = "#000000";
+//         // document.getElementById("blu").style.color = "var(--blue)";
+//     }
+//     if (document.getElementById("pageSwitch").checked == false){
+//         strongColor.style.backgroundColor = "var(--orange)";
+//         footColor.style.backgroundColor = "var(--orange)";
+//         aboutSectionStatus.style.display = "block";
+//         portfolioPageStatus.style.display = "none";
+//         headBlock.style.display = "block";
+//         // document.getElementById("orn").style.color = "var(--orange)";
+//         // document.getElementById("blu").style.color = "#000000";
+//     }
+
+//     console.log(document.getElementById("pageSwitch").checked);
+// }
+
+document.getElementById('aboutTab').onclick = function() {
+    strongColor.style.backgroundColor = "var(--orange)";
+    footColor.style.backgroundColor = "var(--orange)";
+    aboutSectionStatus.style.display = "block";
+    portfolioPageStatus.style.display = "none";
+    contactPageStatus.style.display = "none";
+    aboutTab.className = "orange";
+    contactTab.className = "none"
+    portfolioTab.className = "none";
+}
+
+document.getElementById('portfolioTab').onclick = function() {
+    strongColor.style.backgroundColor = "var(--blue)";
+    footColor.style.backgroundColor = "var(--blue)";
+    aboutSectionStatus.style.display = "none";
+    portfolioPageStatus.style.display = "block";
+    contactPageStatus.style.display = "none";
+    aboutTab.className = "none";
+    contactTab.className = "none"
+    portfolioTab.className = "blue";
+}
+
+document.getElementById('contactTab').onclick = function() {
+    strongColor.style.backgroundColor = "var(--purple)";
+    footColor.style.backgroundColor = "var(--purple)";
+    aboutSectionStatus.style.display = "none";
+    portfolioPageStatus.style.display = "none";
+    contactPageStatus.style.display = "block";
+    aboutTab.className = "none";
+    contactTab.className = "purple"
+    portfolioTab.className = "none";
 }
