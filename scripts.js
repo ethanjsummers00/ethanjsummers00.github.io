0f5f257a2c1abd939c2f73120d261bb1142e4e44
Ethan Summers <ethanjsummers00@gmail.com>
2023-10-30 13:25:12 -0700

    Portfolio Update

--- scripts.js
+++ scripts.js
@@ -12,21 +12,21 @@ var day = (new Date()).getDate();
 var age = 0;
 
 
-$(document).on("scroll", function() {
-    var pageTop = $(document).scrollTop();
-    var pageBottom = pageTop + $(window).height();
-    var tags = $(".tag");
+// $(document).on("scroll", function() {
+//     var pageTop = $(document).scrollTop();
+//     var pageBottom = pageTop + $(window).height();
+//     var tags = $(".tag");
   
-    for (var i = 0; i < tags.length; i++) {
-      var tag = tags[i];
+//     for (var i = 0; i < tags.length; i++) {
+//       var tag = tags[i];
   
-      if ($(tag).position().top < pageBottom) {
-        $(tag).addClass("visible");
-      } else {
-        $(tag).removeClass("visible");
-      }
-    }
-  });
+//       if ($(tag).position().top < pageBottom) {
+//         $(tag).addClass("visible");
+//       } else {
+//         $(tag).removeClass("visible");
+//       }
+//     }
+//   });
 
 function myAge() {
     if (month >= 10) {
